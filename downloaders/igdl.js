3e89b8444603dfbebf3472df552daecc98137bd5
MinatoCodes <subinsirmal11@gmail.com>
2025-08-09 13:36:30 +0545

    Update igdl.js

--- downloaders/igdl.js
+++ downloaders/igdl.js
@@ -3,11 +3,11 @@ const axios = require('axios');
 module.exports = async (url) => {
     const apiUrl = `https://backend1.tioo.eu.org/igdl?url=${encodeURIComponent(url)}`;
     const res = await axios.get(apiUrl, { headers: { 'User-Agent': 'Mozilla/5.0' } });
-    const data = res.data;
+    const data = res;
     return {
         success: true,
         creator: "MinatoCodes",
         platform: "igdl",
-        download_url: data.url || null
+        download_url: data.url[0]|| null
     };
 };
