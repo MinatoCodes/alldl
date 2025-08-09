dadfa333a1b252d3994c530e857034332afed6f2
MinatoCodes <subinsirmal11@gmail.com>
2025-08-09 11:39:39 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,36 @@
-const ttdl = require("./downloaders/ttdl");
-const igdl = require("./downloaders/igdl");
-const fbdown = require("./downloaders/fbdown");
-const ytdl = require("./downloaders/ytdl");
-const tweetdl = require("./downloaders/tweetdl");
-const gdrivedl = require("./downloaders/gdrive");
+// api/alldl.js
+const url = require('url');
+const ttdl = require('../downloaders/ttdl');
+const igdl = require('../downloaders/igdl');
+const fbdown = require('../downloaders/fbdown');
+const ytdl = require('../downloaders/ytdl');
+const tweetdl = require('../downloaders/tweetdl');
+const gdrive = require('../downloaders/gdrive');
 
-module.exports = async function alldl(url) {
-    if (!url) throw new Error("No URL provided");
+const matchers = [
+    { regex: /tiktok\.com/i, handler: ttdl },
+    { regex: /instagram\.com/i, handler: igdl },
+    { regex: /facebook\.com|fb\.watch/i, handler: fbdown },
+    { regex: /youtube\.com|youtu\.be/i, handler: ytdl },
+    { regex: /twitter\.com|x\.com/i, handler: tweetdl },
+    { regex: /drive\.google\.com/i, handler: gdrive }
+];
 
-    const matchers = [
-        // TikTok
-        { regex: /tiktok\.com/i, handler: ttdl },
-
-        // Instagram (reel, post, story)
-        { regex: /instagram\.com/i, handler: igdl },
-
-        // Facebook (fb.watch, m.facebook.com, www.facebook.com)
-        { regex: /(?:facebook\.com|fb\.watch)/i, handler: fbdown },
-
-        // YouTube (shorts, normal watch links)
-        { regex: /(?:youtube\.com|youtu\.be)/i, handler: ytdl },
-
-        // Twitter / X
-        { regex: /(?:twitter\.com|x\.com)/i, handler: tweetdl },
-
-        // Google Drive
-        { regex: /drive\.google\.com/i, handler: gdrivedl }
-    ];
-
-    for (const { regex, handler } of matchers) {
-        if (regex.test(url)) {
-            return await handler(url);
+module.exports = async (req, res) => {
+    try {
+        const queryUrl = req.query.url;
+        if (!queryUrl) {
+            return res.status(400).json({ success: false, error: 'Missing url parameter' });
         }
+
+        const matched = matchers.find(m => m.regex.test(queryUrl));
+        if (!matched) {
+            return res.status(400).json({ success: false, error: 'Unsupported platform' });
+        }
+
+        const data = await matched.handler(queryUrl);
+        res.status(200).json({ success: true, data });
+    } catch (err) {
+        res.status(500).json({ success: false, error: err.message || err });
     }
-
-    throw new Error("No suitable downloader found for this URL");
 };
-
-// Local test
-if (require.main === module) {
-    (async () => {
-        try {
-            const result = await module.exports("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-            console.log(result);
-        } catch (err) {
-            console.error(err.message);
-        }
-    })();
-         }
-         
